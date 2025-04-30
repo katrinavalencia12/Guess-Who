593e0b666dea72cfaccd56d6452055f184ba28c7
Katrina Valencia <169079074+katrinavalencia12@users.noreply.github.com>
2025-04-30 14:34:31 -0400

    added files

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GuessWho;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ startPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace GuessWho;
+
+public partial class startPage : ContentPage
+{
+	public startPage()
+	{
+		InitializeComponent();
+	}
+
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+		var button = sender as Button;
+		if (button.Text.Contains("2"))
+		{
+			Navigation.PushAsync(new HomePage());
+		}
+    }
+}
